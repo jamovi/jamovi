9d6cbf95c0124c5e8f3c0079a4316c1f2db52f20
dropmann <damian_dropmann@me.com>
2016-10-10 17:13:26 +1100

    Added filter icons to variable lists
    NOTE: does not stop you from adding an incorrect variable yet.

--- client/analysisui/gridvariablestargetlist.js
+++ client/analysisui/gridvariablestargetlist.js
@@ -3,21 +3,29 @@
 var $ = require('jquery');
 var _ = require('underscore');
 var FormatDef = require('./formatdef');
-var EnumPropertyFilter = require('./enumpropertyfilter');
+var EnumArrayPropertyFilter = require('./enumarraypropertyfilter');
 var GridTargetList = require('./gridtargetlist');
 var Overridable = require('./overridable');
 
 var GridVariablesTargetList = function(params) {
-    GridTargetList.extend(this, params);
+    GridTargetList.extendTo(this, params);
     Overridable.extendTo(this);
 
-    this.registerSimpleProperty("variableFilter", "none", new EnumPropertyFilter(["none", "unique", "unique_per_row", "unique_per_column"], "none"));
+    this.registerSimpleProperty("variableFilter", [], new EnumArrayPropertyFilter(["none", "continuous", "ordinal", "nominal", "nominaltext"]));
 
     var self = this;
     this._override('onRenderToGrid', function(baseFunction, grid, row, column) {
-        var returnValue = baseFunction.call(self.targetGrid, grid, row, column);
+        var returnValue = baseFunction.call(self, grid, row, column);
         this.targetGrid.$el.addClass("silky-variable-target");
 
+        var allowableVariableTypes = this.getPropertyValue("variableFilter");
+        if (allowableVariableTypes.length > 0) {
+            var $icons = $('<div class="silky-variablelist-icons"></div>');
+            for (let i = 0; i < allowableVariableTypes.length; i++) {
+                $icons.append('<div style="display: inline-block; overflow: hidden;" class="silky-variable-type-img silky-variable-type-' + allowableVariableTypes[i] + '"></div>');
+            }
+            this.targetGrid.$el.append($icons);
+        }
         return returnValue;
     });
 };
