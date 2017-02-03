055e6d50e851e12f5bd4d303dd9a783457d68865
dropmann <damian_dropmann@me.com>
2017-02-03 22:22:26 +1100

    Fixed travis issues

--- server/jamovi/server/instance.py
+++ server/jamovi/server/instance.py
@@ -274,14 +274,14 @@ class Instance:
         try:
             file_exists = os.path.isfile(path)
             successfulWrite = False
-            if file_exists == False or request.override == True:
+            if file_exists is False or request.override is True:
                 formatio.write(self._data, path)
                 successfulWrite = True
 
             self._filepath = path
             response = jcoms.SaveProgress()
-            response.fileExists = file_exists;
-            response.success = successfulWrite;
+            response.fileExists = file_exists
+            response.success = successfulWrite
             self._coms.send(response, self._instance_id, request)
 
             if response.success:
