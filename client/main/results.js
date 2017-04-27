8a0c269e093c9cce79194c889438668c6f4f481a
Damian Dropmann <damian_dropmann@me.com>
2017-04-27 19:49:22 +1000

    Added welcome screen
    * small change - on to once

--- client/main/results.js
+++ client/main/results.js
@@ -30,6 +30,19 @@ var ResultsView = Backbone.View.extend({
             model: this.model,
             mode: 'text' });
 
+        this.$welcome = $('<iframe id="main_welcome" \
+                name="welcome" \
+                sandbox="allow-scripts allow-same-origin" \
+                src="https://jamovi.org/welcome.html?version=0.7.3.0" \
+                class="silky-welcome-panel" \
+                style="overflow: hidden; box-sizing: border-box;" \
+                ></iframe>');
+        this.$welcome.appendTo(this.$el);
+
+        this.model.once("change:selectedAnalysis", (event) => {
+            this.$welcome.addClass('silky-welcome-panel-hidden');
+        });
+
         this.model.on('change:resultsMode', event => {
             if (event.changed.resultsMode === 'text')
                 this.$textView.removeClass('silky-results-panel-hidden');
