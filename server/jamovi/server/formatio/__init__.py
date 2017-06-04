e08f52f9f2bb5524193d243f88c73ffbea9e60f3
Jonathon Love <jon@thon.cc>
2017-06-05 09:14:32 +1000

    Import: capitalised file extensions (i.e. .CSV) now handled

--- server/jamovi/server/formatio/__init__.py
+++ server/jamovi/server/formatio/__init__.py
@@ -14,11 +14,13 @@ def read(data, path, is_example=False):
     else:
         data.title = os.path.basename(path)
 
+    ext = os.path.splitext(path)[1].lower()
+
     if path == '':
         blank.read(data)
-    elif path.endswith('.csv') or path.endswith('.txt'):
+    elif ext == '.csv' or ext == '.txt':
         csv.read(data, path)
-    elif path.endswith('.jasp'):
+    elif ext == '.jasp':
         jasp.read(data, path)
     else:
         omv.read(data, path, is_example)
@@ -27,17 +29,23 @@ def read(data, path, is_example=False):
 
 
 def write(data, path):
-    if path.endswith('.csv'):
+
+    ext = os.path.splitext(path)[1].lower()
+
+    if ext == '.csv':
         csv.write(data, path)
     else:
         omv.write(data, path)
 
 
-def is_supported(filename):
-    return (filename.endswith('.csv') or
-            filename.endswith('.txt') or
-            filename.endswith('.omv') or
-            filename.endswith('.jasp') or
-            filename.endswith('.pdf') or
-            filename.endswith('.html') or
-            filename.endswith('.htm'))
+def is_supported(path):
+
+    ext = os.path.splitext(path)[1].lower()
+
+    return (ext == '.csv' or
+            ext == '.txt' or
+            ext == '.omv' or
+            ext == '.jasp' or
+            ext == '.pdf' or
+            ext == '.html' or
+            ext == '.htm')
