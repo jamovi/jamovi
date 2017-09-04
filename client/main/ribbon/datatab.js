85d1af674cd25b300cca9cfae7e5dbd124e8422b
raviselker <selker.ravi@gmail.com>
2017-09-04 15:44:20 +0200

    Data menu: added compute buttons

--- client/main/ribbon/datatab.js
+++ client/main/ribbon/datatab.js
@@ -26,8 +26,14 @@ const DataTab = function() {
                 new RibbonButton({ title: 'Setup', name: 'editVar', size: 'large' }),
                 new RibbonGroup({ orientation: 'vertical', items: [
                     new RibbonButton({ title: 'Add', name: 'addVar', subItems: [
-                        new RibbonButton({ title: 'Insert', name: 'insertVar' }),
-                        new RibbonButton({ title: 'Append', name: 'appendVar' }),
+                        new RibbonGroup({ title: 'Data Variable', orientation: 'vertical', items: [
+                            new RibbonButton({ title: 'Insert', name: 'insertVar' }),
+                            new RibbonButton({ title: 'Append', name: 'appendVar' })
+                        ]}),
+                        new RibbonGroup({ title: 'Computed Variable', orientation: 'vertical', items: [
+                            new RibbonButton({ title: 'Insert', name: 'insertComputed' }),
+                            new RibbonButton({ title: 'Append', name: 'appendComputed' })
+                        ]}),
                     ]}),
                     new RibbonButton({ title: 'Delete', name: 'delVar' }),
                 ]}),
