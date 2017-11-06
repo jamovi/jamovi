jamovi/jamovi

commit f28d8baffe69f8b28c8a09e1126140a26b38d67f

Fix to scrollbar behaviour
Fixes #349

client/main/tableview.js
@@ -71,7 +71,7 @@ const TableView = SilkyView.extend({
 
         this.selection = null;
 
-        this.$el.on('mousedown', event => this._mouseDown(event));
+        this.$body.on('mousedown', event => this._mouseDown(event));
         this.$el.on('mousemove', event => this._mouseMove(event));
         $(document).on('mouseup', event => this._mouseUp(event));
         this.$el.on('dblclick', event => this._dblClickHandler(event));

commit 8895736e671fdf64a7d650780855c930c8a1aaef

Fix to column selection
Fix after breakage from f28d8baffe69f8b28c8a09e1126140a26b38d67f when
fixing #349

client/main/tableview.js
@@ -72,6 +72,7 @@ const TableView = SilkyView.extend({
         this.selection = null;
 
         this.$body.on('mousedown', event => this._mouseDown(event));
+        this.$header.on('mousedown', event => this._mouseDown(event));
         this.$el.on('mousemove', event => this._mouseMove(event));
         $(document).on('mouseup', event => this._mouseUp(event));
         this.$el.on('dblclick', event => this._dblClickHandler(event));