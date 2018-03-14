15600d88e12260483942297c8f5df16d2c934a7b
Jonathon Love <jon@thon.cc>
2018-03-14 17:56:52 +1100

    Fix to configure.bat - encode Makefile with UTF-8

--- engine/configure.bat
+++ engine/configure.bat
@@ -1,2 +1,2 @@
 
-powershell -Command "(gc Makefile.in) -replace '%%PREFIX%%', '' -replace '%%LIBDIR%%', '' -replace '%%CFLAGS%%', '' -replace '%%CXXFLAGS%%', '' -replace '%%R_PATH%%', '' -replace '%%BASE_MODULE_PATH%%', '' -replace '%%R_HOME%%', '' | Out-File Makefile"
+powershell -Command "(gc Makefile.in) -replace '%%PREFIX%%', '' -replace '%%LIBDIR%%', '' -replace '%%CFLAGS%%', '' -replace '%%CXXFLAGS%%', '' -replace '%%R_PATH%%', '' -replace '%%BASE_MODULE_PATH%%', '' -replace '%%R_HOME%%', '' | Out-File Makefile" -encoding utf8
