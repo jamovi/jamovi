90f2a3a19254addd3aad8f04ceab762c9ca79172
Jonathon Love <jon@thon.cc>
2020-04-17 11:12:58 +1000

    Added QP support

---
+++ server/jamovi/server/integrations/base.py
@@ -0,0 +1,19 @@
+
+class IntegrationHandler:
+    async def read(self, response):
+        pass
+
+    async def process(self, data):
+        pass
+
+    async def save(self, content):
+        pass
+
+    def gen_message(self):
+        return None
+
+    def get_title(self):
+        return ''
+
+    def is_for(self, path):
+        return False
