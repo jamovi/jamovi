e1072d2ef4d1ee29d67fef5bab44f4430ae57c85
Jonathon Love <jon@thon.cc>
2020-04-21 12:24:43 +1000

    Tidy up of engine

--- engine/engine/enginer.h
+++ engine/engine/enginer.h
@@ -64,8 +64,7 @@ private:
 
     Rcpp::Environment create(const jamovi::coms::AnalysisRequest &analysis);
 
-    void sendResults(bool incAsText, bool complete);
-    void sendResults2(Rcpp::Environment &ana, bool incAsText, bool complete);
+    void sendResults(Rcpp::Environment &ana, bool incAsText, bool complete);
 
     static void createDirectories(const std::string &path);
     static void setLibPaths(const std::string &moduleName);
