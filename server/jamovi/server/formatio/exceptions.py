123a10e12f4bddea05a2afea744a22b09995891f
Jonathon Love <jon@thon.cc>
2020-12-12 18:02:20 +1100

    Added file corrupt error to .xlsx import

---
+++ server/jamovi/server/formatio/exceptions.py
@@ -0,0 +1,10 @@
+
+class FileIOError(Exception):
+    pass
+
+class FileReadError(FileIOError):
+    pass
+
+class FileCorruptError(FileReadError):
+    def __init__(self, message='File is corrupt'):
+        super().__init__(message)
