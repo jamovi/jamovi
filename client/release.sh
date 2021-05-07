694a4ea487de4f20e659ab484080260ba2dc7b73
Jonathon Love <jon@thon.cc>
2021-05-07 18:26:42 +1000

    Tweak to release client build

--- client/release.sh
+++ client/release.sh
@@ -1,23 +1,33 @@
-#!/bin/sh
+#!/bin/bash
+
+mkdir -p www
+rm -rf www/*
+mkdir -p www/assets
+
+cp *.html www/
+cp *.css  www/
+cp *.js   www/
+
 
 for file in assets/*; do
     crc=`crc32 "$file"`
-    for css_file in *.css; do
-        sed --in-place "s|$file\(\?v=[a-z0-9]\{8\}\)\?|$file?v=$crc|g" "$css_file"
+    base=$(basename $file)
+    name=${base%.*}
+    ext=${base##*.}
+    cp $file www/assets/$name-$crc.$ext
+    for mod_file in www/*.{css,js}; do
+        sed --in-place "s|$file\(\?v=[a-z0-9]\{8\}\)\?|assets/$name-$crc.$ext|g" "$mod_file"
     done
 done
 
 # rename the css files with their crc
-for file in *.css; do
+for file in *.{css,js}; do
     crc=`crc32 "$file"`
-    for html_file in *.html; do
-        sed --in-place "s|$file\(\?v=[a-z0-9]\{8\}\)\?|$file?v=$crc|g" "$html_file"
-    done
-done
-
-for file in *.js; do
-    crc=`crc32 "$file"`
-    for html_file in *.html; do
-        sed --in-place "s|$file\(\?v=[a-z0-9]\{8\}\)\?|$file?v=$crc|g" "$html_file"
+    base=$(basename $file)
+    name=${base%.*}
+    ext=${base##*.}
+    mv www/$base www/$name-$crc.$ext
+    for mod_file in www/*.html; do
+        sed --in-place "s|$file\(\?v=[a-z0-9]\{8\}\)\?|$name-$crc.$ext|g" "$mod_file"
     done
 done
