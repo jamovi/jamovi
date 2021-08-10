64e9605423899d6067308ab8c56608bf7fcbe9fc
Jonathon Love <jon@thon.cc>
2021-08-10 12:42:56 +1000

    Tidy-up of engine coms

--- engine/engine/enginecoms.cpp
+++ engine/engine/enginecoms.cpp
@@ -8,12 +8,15 @@
 #include <iostream>
 #include <cstdlib>
 
+#include <nanomsg/nn.h>
+#include <nanomsg/pair.h>
+
 #include "jamovi.pb.h"
 
 using namespace std;
 using namespace jamovi::coms;
 
-class MemoryBuffer : public std::streambuf
+class MemoryBuffer : public streambuf
 {
 public:
     MemoryBuffer(char* data, int len) {
@@ -26,38 +29,71 @@ EngineComs::EngineComs()
     GOOGLE_PROTOBUF_VERIFY_VERSION;
 }
 
-void EngineComs::parse(char *data, int len)
+void EngineComs::connect(const string &path)
+{
+    _socket = nn_socket(AF_SP, NN_PAIR);
+    if (_socket < 0)
+        throw runtime_error("Unable to connect : could not create socket");
+
+    int timeout;
+
+    timeout = 1500;
+    nn_setsockopt(_socket, NN_SOL_SOCKET, NN_SNDTIMEO, &timeout, sizeof(timeout));
+
+    timeout = 500;
+    nn_setsockopt(_socket, NN_SOL_SOCKET, NN_RCVTIMEO, &timeout, sizeof(timeout));
+
+    _connId = nn_connect(_socket, path.c_str());
+    if (_connId < 0)
+        throw runtime_error("Unable to connect : could not connect to endpoint");
+}
+
+void EngineComs::send(const string &results, bool complete)
 {
     ComsMessage message;
 
-    MemoryBuffer buf1(data, len);
-    istream is1(&buf1);
+    message.set_payload(results);
+    message.set_payloadtype("AnalysisResponse");
+    message.set_status(complete ? Status::COMPLETE : Status::IN_PROGRESS);
 
-    if ( ! message.ParseFromIstream(&is1))
-    {
-        cerr << "EngineComs::parse(); failed to parse message\n";
-        cerr.flush();
-        return;
-    }
+    string data;
+    message.SerializeToString(&data);
+    nn_send(_socket, data.data(), data.size(), 0);
+}
 
+AnalysisRequest EngineComs::read()
+{
     AnalysisRequest request;
 
-    string payload = message.payload();
-    MemoryBuffer buf2((char*)payload.c_str(), payload.size());
-    istream is2(&buf2);
-
-    if ( ! request.ParseFromIstream(&is2))
+    while (true)
     {
-        cerr << "EngineComs::parse(); failed to parse request\n";
-        cerr.flush();
-        return;
+        char *buf = NULL;
+        int nbytes = nn_recv(_socket, &buf, NN_MSG, 0);
+
+        if (nbytes >= 0)
+        {
+            ComsMessage message;
+            MemoryBuffer buf1(buf, nbytes);
+            istream is1(&buf1);
+
+            if (message.ParseFromIstream(&is1))
+            {
+                string payload = message.payload();
+                MemoryBuffer buf2((char*)payload.c_str(), payload.size());
+                istream is2(&buf2);
+
+                request.ParseFromIstream(&is2);
+                break;
+            }
+
+            nn_freemsg(buf);
+        }
     }
 
-    if (request.restartengines())
-    {
-        restartRequested();
-        return;
-    }
+    return request;
+}
 
-    analysisRequested(message.id(), request);
+void EngineComs::close()
+{
+    nn_term();
 }
