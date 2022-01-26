9b04e0aaaaef862eec3316ca349dccdcfe5e10b9
Jonathon Love <jon@thon.cc>
2022-01-26 13:21:11 +1100

    Tweak to some exceptions

--- server/jamovi/server/formatio/exceptions.py
+++ server/jamovi/server/formatio/exceptions.py
@@ -14,10 +14,14 @@ class FileWriteError(FileIOError):
 
 
 class FileCorruptError(FileReadError):
-    def __init__(self, message=_('File is corrupt')):
+    def __init__(self, message=None):
+        if message is None:
+            message = _('File is corrupt')
         super().__init__(message)
 
 
 class FileFormatNotSupportedError(FileReadError):
-    def __init__(self, message=_('File format is not supported')):
+    def __init__(self, message=None):
+        if message is None:
+            message = _('File format is not supported')
         super().__init__(message)
