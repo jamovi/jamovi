53917472f06b571854f908d327f14e5ad8f00f9e
Jonathon Love <jon@thon.cc>
2022-04-25 14:32:58 +1000

    Added announcements

---
+++ client/main/misc/announcements.js
@@ -0,0 +1,3 @@
+
+_('jamovi is now available in { insert translation language }! (Many thanks to @{ insert your twitter handle here (or put a more generic "the jamovi translation team", if you don\'t want credit) })')
+_('jamovi is now available in { insert translation language }! (Many thanks to { insert your name here (or put a more generic "the jamovi translation team", if you don\'t want credit) })')
