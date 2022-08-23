42d00ffe293d530240454fc17ced355afd73c233
Jonathon Love <jon@thon.cc>
2022-08-23 11:43:36 +1000

    Improvement to copy/paste of html table content

--- client/common/utils.js
+++ client/common/utils.js
@@ -3,6 +3,10 @@
 
 module.exports = {
     s6e: function(x) {
-        return x.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+        return x.replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#039;');
     }
 };
