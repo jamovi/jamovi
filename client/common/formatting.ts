df32c417c461bbd4ed18af8a4311b3ab78934b60
Jonathon Love <jon@thon.cc>
2025-10-17 12:12:21 +1100

    Added support for euro dec in results tables

--- client/common/formatting.ts
+++ client/common/formatting.ts
@@ -8,7 +8,7 @@ export const determFormat = function(values, type, format, settings, maxNS?, min
     if (format === undefined)
         format = '';
     if (settings === undefined)
-        settings = { 't': 'sf', 'n': 3, 'pt': 'dp', 'p': 3 };
+        settings = { 't': 'sf', 'n': 3, 'pt': 'dp', 'p': 3, 'ds': '.' };
     if (minNS === undefined)
         minNS = 1e-3;
     if (maxNS === undefined)
@@ -38,7 +38,7 @@ export const determFormat = function(values, type, format, settings, maxNS?, min
                 minAbsNS = absValue;
         }
         else {
-            let exponent = Math.floor(Math.log10(absValue));
+            let exponent = Math.log10(absValue) | 0;
             let absS = Math.abs(exponent);
             if (absS !== 0 && isFinite(absS) && absS > maxAbsExpnt)
                 maxAbsExpnt = absS;
@@ -48,8 +48,9 @@ export const determFormat = function(values, type, format, settings, maxNS?, min
     let dp, sf;
     let lz = true; // leading zero
 
-    let t = settings.t;
-    let n = settings.n;
+    let { t } = settings;
+    let { n } = settings;
+    const ds = settings.ds || '.';
 
     if (formats.includes('pvalue')) {
         t = settings.pt;
@@ -106,9 +107,9 @@ export const determFormat = function(values, type, format, settings, maxNS?, min
 
     let expw = 0;
     if (isFinite(maxAbsExpnt))
-        expw = Math.floor(Math.log10(maxAbsExpnt) + 1);
+        expw = (Math.log10(maxAbsExpnt) | 0) + 1;
 
-    return { dp, expw, format, sf, maxNS, minNS, t, lz };
+    return { dp, expw, format, sf, maxNS, minNS, t, lz, ds };
 };
 
 export const format = function(value, fmt) {
@@ -134,27 +135,28 @@ export const format = function(value, fmt) {
     }
 
     if (fmt.t === 'dp' && fmt.format.includes('pvalue') && value < Math.pow(10, -fmt.dp)) {
-        return '<\u2009' + Math.pow(10, -fmt.dp).toFixed(fmt.dp).substring(1);
+        return '<\u2009' + Math.pow(10, -fmt.dp).toFixed(fmt.dp).replace('.', fmt.ds).substring(1);
     }
     else if (fmt.format.includes('pc')) {
-        return '' + (100 * value).toFixed(fmt.dp - 2) + '\u2009%';
+        return '' + (100 * value).toFixed(fmt.dp - 2).replace('.', fmt.ds) + '\u2009%';
     }
     else if (Math.abs(value) >= fmt.minNS && Math.abs(value) <= fmt.maxNS) {
         let str = value.toFixed(fmt.dp);
         if (fmt.lz === false && str.startsWith('0.'))
             str = str.substring(1);
+        str = str.replace('.', fmt.ds)
         return str;
     }
     else {
         const exponent = Math.floor(Math.log10(Math.abs(value)));
         const mantissa = value/Math.pow(10, exponent);
         if (value === 0)
-            return value.toFixed(fmt.dp);
+            return value.toFixed(fmt.dp).replace('.', fmt.ds);
         const expSign = Math.abs(value) < 1 ? '-' : '+';
         let spaces = fmt.expw - Math.floor(Math.log10(Math.abs(exponent)));
         spaces = Math.max(spaces, 0);
         const gap = Array(spaces).join(' ');
-        return mantissa.toFixed(fmt.sf - 1) + 'e' + gap + expSign + Math.abs(exponent);
+        return mantissa.toFixed(fmt.sf - 1).replace('.', fmt.ds) + 'e' + gap + expSign + Math.abs(exponent);
     }
 
 };
