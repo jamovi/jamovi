d452da9a8813daeea13f9436154e2758f1f900b9
Jonathon Love <jon@thon.cc>
2025-10-23 11:38:36 +1100

    Updated opensesame example

--- opensesame/R/opensesame.b.R
+++ opensesame/R/opensesame.b.R
@@ -9,11 +9,10 @@ openSesameClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
 
             if (self$options$open) {
 
-                # a matching results object is created in results
-                # (however you can't use self$results$open, but rather
-                # self$results$get('open'). this object contains a params
-                # object which contains a fullPath and a path)
-                params <- self$results$get('open')$params
+                open <- self$results$get('open')
+                params <- open$params
+
+                print(params)
 
                 # write the file to the fullPath
                 write.csv(datasets::ToothGrowth, file=params$fullPath, row.names=FALSE)
