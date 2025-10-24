e90753eaedb9d7d588773a2ebde8608c45a7b07f
Jonathon Love <jon@thon.cc>
2025-10-24 12:06:55 +1100

    Updated example

--- opensesame/R/opensesame.b.R
+++ opensesame/R/opensesame.b.R
@@ -10,26 +10,30 @@ openSesameClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
             if (self$options$open) {
 
                 option <- self$options$option('open')
-
-                for (i in 1:3) {
-                    action <- option$perform()
-
+                option$perform(function(action) {
                     print(action$params)
 
                     # write the file to the fullPath
                     write.csv(ToothGrowth, file=action$params$fullPath, row.names=FALSE)
                     ToothGrowth <- rev(ToothGrowth)
 
-                    # construct a result object using params$path
-                    result <- list(
+                    # return a result object using params$path
+                    list(
                         path = action$params$path,
                         title = 'the fish was delish',
                         ext = 'csv'
                     )
+                })
+            }
 
-                    # set the result
-                    action$setResult(result)
-                }
+            x <- 7
+
+            if (self$options$error) {
+                option <- self$options$option('error')
+                option$perform(function(action) {
+                    print(x)
+                    bruce()  # function which doesn't exist
+                })
             }
 
         })
