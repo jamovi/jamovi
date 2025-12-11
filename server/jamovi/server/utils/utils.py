aa8bbaeba324e250fa34dd2b657eec80ce7efe11
Jonathon Love <jon@thon.cc>
2025-12-11 18:40:57 +1100

    Adjustments

--- server/jamovi/server/utils/utils.py
+++ server/jamovi/server/utils/utils.py
@@ -1,52 +1,68 @@
 
-def parse_number(input_str):
-    s = input_str.strip().replace(" ", "")
+import re
+
+from typing import Literal
+
+
+UNAMBIGUOUS_EURO_INT = re.compile(r"^-?[1-9]\d{0,2}(?:\.\d{3}){2,}$")
+UNAMBIGIOUS_INT = re.compile(r"^-?[1-9]\d{0,2}(?:,\d{3}){2,}$")
+
+# # Matches
+# 1,234,567
+# 12,345,678
+# 123,456,789
+# 1,234,567,890
+# 12,345,678,901,234
+
+# # Won't match
+# 999,999
+
+
+def parse_number(input_str: str, dec_symbol: Literal['.', ',']='.') -> float | int | str:
+
+    input_str = input_str.strip()
+
+    s = input_str.replace(" ", "")
     comma = "," in s
     dot = "." in s
 
-    if comma and dot:
-        last_comma = s.rfind(",")
-        last_dot = s.rfind(".")
+    try:
+        if comma and dot:
+            last_comma = s.rfind(",")
+            last_dot = s.rfind(".")
 
-        if last_comma > last_dot:
-            s = s.replace(".", "").replace(",", ".")
-        else:
-            s = s.replace(",", "")
-
-        try:
-            value = float(s)
-            return value
-        except ValueError:
-            pass
-    
-    elif comma:
-            is_int = True
-            parts = s.split(",")
-
-            if len(parts) >= 2:
-                for i, part in enumerate(parts):
-                    if (i == 0 and len(part) > 3) or (i > 0 and len(part) != 3):
-                        is_int = False
-                        break
-
-            if is_int:
+            if last_comma > last_dot:
+                # 123.456,78 => 123456.78
+                s = s.replace(".", "").replace(",", ".")
+            else:
+                # 123,456.78 => 123456.78
                 s = s.replace(",", "")
-                try:
-                    value = int(s)
-                    return value
-                except ValueError:
-                    pass
-            elif len(parts) == 2:
-                s = s.replace(",", ".")
-                try:
-                    value = float(s)
-                    return value
-                except ValueError:
-                    pass
 
+            return float(s)
 
-    value = input_str
-    if isinstance(input_str, str):
-        value = value.strip()
+        elif comma:
+            if UNAMBIGIOUS_INT.match(s):
+                # i.e. 1,234,567 => 1234567
+                s = s.replace(",", "")
+                return int(s)
+            else:
+                if dec_symbol == ",":
+                    # i.e. 123,45 => 123.45
+                    s = s.replace(",", ".")
+                    return float(s)
+                else:
+                    # i.e. 123,45 => 12345
+                    s = s.replace(",", "")
+                    return int(s)
 
-    return value
+        elif dot:
+            if dec_symbol == "," and UNAMBIGUOUS_EURO_INT.match(s):
+                # i.e. 1.234.567 => 1234567
+                s = s.replace(".", "")
+                return int(s)
+            else:
+                return float(s)
+    except ValueError:
+        pass
+
+    return input_str
