97e09bad6ae90c723a8f1e90a56a825b96b14599
Jonathon Love <jon@thon.cc>
2026-06-01 20:35:31 +1000

    client: backstage: simplified extras

---
+++ client/main/backstage/extras.ts
@@ -0,0 +1,8 @@
+
+import type { IBackstageExtras } from './types';
+
+export const extras: IBackstageExtras = {
+    init: (_backstage) => {},
+    getOpenPlaces: () => [],
+    getSaveAsPlaces: () => [],
+};
