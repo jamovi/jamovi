52966643a2ccb82c122602f16d84fb42422235ae
Jonathon Love <jon@thon.cc>
2026-06-01 15:11:28 +1000

    server: migrate tornado -> aiohttp

--- server/jamovi/server/downloader.py
+++ server/jamovi/server/downloader.py
@@ -1,21 +1,13 @@
 
-import re
-import os.path as path
+import ssl
+from asyncio import create_task
+from functools import partial
 from tempfile import TemporaryFile
+
+import aiohttp
 import certifi
 
-
-from functools import partial
-from asyncio import create_task
-
-from tornado.simple_httpclient import SimpleAsyncHTTPClient as AsyncHTTPClient
-
-from .utils import conf
 from .utils.stream import ProgressStream
-
-from tornado.httpclient import HTTPError as TornadoHTTPError
-from ssl import SSLCertVerificationError
-
 from .i18n import _
 
 
@@ -40,41 +32,23 @@ class CaptivePortalError(DownloadError):
         super().__init__(message)
 
 
-class DownloadInfo:
-    def __init__(self, response, progress, size, file, stream):
-        self.response = response
-        self.progress = progress
-        self.size = size
+class _DownloadInfo:
+    def __init__(self, file, stream):
+        self.progress = 0
+        self.size = -1
         self.file = file
         self.stream = stream
 
 
 class Download:
     def __init__(self):
-        self._chain_path = certifi.where()
-        self._client = AsyncHTTPClient(max_body_size=512 * 1024 * 1024)
+        self._ssl_context = ssl.create_default_context(cafile=certifi.where())
 
     def download(self, url, io=None):
-
-        info = DownloadInfo(None, 0, -1, io, ProgressStream())
-
-        header_callback = partial(self._header_callback, info)
-        streaming_callback = partial(self._streaming_callback, info)
-        complete_callback = partial(self._complete, info)
-
-        response = self._client.fetch(
-            url,
-            header_callback=header_callback,
-            streaming_callback=streaming_callback,
-            request_timeout=24 * 60 * 60,
-            ca_certs=self._chain_path)
-
-        info.response = response
-
+        info = _DownloadInfo(io, ProgressStream())
         info.stream.write((0, 1))
-        task = create_task(self._download(info))
-        task.add_done_callback(complete_callback)
-
+        task = create_task(self._download(url, info))
+        task.add_done_callback(partial(self._complete, info))
         return info.stream
 
     def _complete(self, info, result):
@@ -83,40 +57,42 @@ class Download:
         except Exception as e:
             info.stream.set_exception(e)
 
-    async def _download(self, info):
-
+    async def _download(self, url, info):
         try:
-            await info.response
-            info.file.flush()
-            info.file.seek(0)
+            async with aiohttp.ClientSession() as session:
+                async with session.get(
+                    url,
+                    ssl=self._ssl_context,
+                    timeout=aiohttp.ClientTimeout(total=24 * 60 * 60),
+                ) as response:
+                    response.raise_for_status()
+                    content_length = response.headers.get('Content-Length')
+                    if content_length is not None:
+                        info.size = int(content_length)
+                    async for chunk in response.content.iter_chunked(64 * 1024):
+                        if info.file is None:
+                            info.file = TemporaryFile()
+                        info.file.write(chunk)
+                        info.progress += len(chunk)
+                        info.stream.write((info.progress, info.size))
+            if info.file is not None:
+                info.file.flush()
+                info.file.seek(0)
             info.stream.set_result(info.file)
-        except SSLCertVerificationError as e:
+        except aiohttp.ClientSSLError as e:
             try:
-                # check for captive portal
-                response = await self._client.fetch(
-                    'http://clients3.google.com/generate_204')
-                if response.code != 204:
-                    raise CaptivePortalError from e
-                else:
-                    raise DownloadError from e
-            except OSError:
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(
+                        'http://clients3.google.com/generate_204'
+                    ) as response:
+                        if response.status != 204:
+                            raise CaptivePortalError from e
+                        else:
+                            raise DownloadError from e
+            except (aiohttp.ClientConnectorError, OSError):
                 raise NoNetworkError from e
-        except OSError as e:
-            raise NoNetworkError from e
-
-
-    def _header_callback(self, info, line):
-        match = re.match(r'Content-Length:[\s]*([0-9]+)', line)
-        if match:
-            info.size = int(match.group(1))
-
-    def _streaming_callback(self, info, chunk):
-        if info.file is None:
-            info.file = TemporaryFile()
-
-        info.file.write(chunk)
-        info.progress += len(chunk)
-        info.stream.write((info.progress, info.size))
+        except (aiohttp.ClientConnectorError, OSError):
+            raise NoNetworkError
 
 
 class Downloader:
