1cb3dfc25366ed6e66453c831e070ef5a53063fe
Jonathon Love <jon@thon.cc>
2026-06-25 10:06:34 +1000

    Tighten welcome sandboxing

--- client/main/results.ts
+++ client/main/results.ts
@@ -97,7 +97,7 @@ export class ResultsView extends HTMLElement {
 
         const iframe = document.createElement('iframe');
         iframe.classList.add('jmv-welcome-iframe');
-        iframe.sandbox = 'allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox';
+        iframe.sandbox = 'allow-scripts';
         // hidden to begin with, only show if successful
         iframe.style.display = 'none';
 
